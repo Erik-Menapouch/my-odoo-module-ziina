276f77b5f8081c2480285cb74d2208cf34c2b1ce
Erik-Menapouch <erik@menapouch.com>
2026-04-27 17:15:35 +0400

    Update main.py

--- website_order_notes/controllers/main.py
+++ website_order_notes/controllers/main.py
@@ -5,12 +5,11 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleOrderNote(WebsiteSale):
 
     @http.route('/shop/checkout', type='http', auth='public', website=True)
-    def checkout(self, **post):
+    def shop_checkout(self, try_skip_step=None, **post):
         if post.get('customer_note') is not None:
             order = request.website.sale_get_order()
             if order:
                 order.sudo().write({
                     'customer_note': post.get('customer_note', '')
                 })
-        response = super(WebsiteSaleOrderNote, self).checkout(**post)
-        return response
+        return super(WebsiteSaleOrderNote, self).shop_checkout(try_skip_step=try_skip_step, **post)
