24144e8242ea380d6e7004c0c0f0e2aaf4e6ef65
Erik-Menapouch <erik@menapouch.com>
2026-04-27 21:39:43 +0400

    Update main.py

--- website_order_notes/controllers/main.py
+++ website_order_notes/controllers/main.py
@@ -1,20 +1,12 @@
-import json
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class WebsiteSaleOrderNote(WebsiteSale):
 
-    @http.route('/shop/checkout', type='http', auth='public', website=True)
-    def shop_checkout(self, try_skip_step=None, **post):
-        return super(WebsiteSaleOrderNote, self).shop_checkout(try_skip_step=try_skip_step, **post)
-
-    @http.route('/shop/address', type='http', auth='public', website=True, sitemap=False)
-    def shop_address_submit(self, **post):
-        if post.get('customer_note') is not None:
-            order = request.website.sale_get_order()
-            if order:
-                order.sudo().write({
-                    'customer_note': post.get('customer_note', '')
-                })
-        return super(WebsiteSaleOrderNote, self).shop_address_submit(**post)
+    @http.route('/shop/save_note', type='http', auth='public', website=True, csrf=False)
+    def save_order_note(self, customer_note='', **post):
+        order = request.cart
+        if order:
+            order.sudo().write({'customer_note': customer_note})
+        return 'ok'
